c391d2ff2eadc1846a076fc5ba629443f7cb445c
itch2 <itch2@Cuidadolap>
2021-04-20 02:38:18 -0600

    Primer programa, hola mundo.

---
+++ Ejer_POO_Gpo_C_Git_Hub.java/src/ejer_poo_gpo_c_git_hub/java/Ejer_POO_Gpo_C_Git_HubJava.java
@@ -0,0 +1,13 @@
+
+package ejer_poo_gpo_c_git_hub.java;
+
+
+public class Ejer_POO_Gpo_C_Git_HubJava {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola soy Carlos Guardado");
+        System.out.println("Estoy conociendo GitHub");
+    }
+    
+}
